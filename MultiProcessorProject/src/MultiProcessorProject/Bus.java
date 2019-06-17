7b0b4db292b90f7e98bdbbfec979b20a9a92f521
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2019-06-17 12:05:03 -0500

    Created required new components
    Co-Authored-By: Krishna Teja Ayinala <kteja-ayinala@users.noreply.github.com>

---
+++ MultiProcessorProject/src/MultiProcessorProject/Bus.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package MultiProcessorProject;
+
+/**
+ * @author {Krishna Teja Ayinala, Sindhura Bonthu}
+ *
+ */
+
+// conncetion between nodes and the memory
+public class Bus {
+
+}
---
+++ MultiProcessorProject/src/MultiProcessorProject/BusController.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package MultiProcessorProject;
+
+/**
+ * @author {Krishna Teja Ayinala, Sindhura Bonthu}
+ *
+ */
+public class BusController {
+	// handle transaction and maintains status
+}
