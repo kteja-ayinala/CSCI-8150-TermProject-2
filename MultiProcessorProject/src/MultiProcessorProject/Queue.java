526a12ab82838f9df0d1f6be0bba11292b8c42c9
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2019-06-21 21:44:15 -0500

    Updated queue to implement roundrobin scheduling

--- MultiProcessorProject/src/MultiProcessorProject/Queue.java
+++ MultiProcessorProject/src/MultiProcessorProject/Queue.java
@@ -8,14 +8,15 @@ package MultiProcessorProject;
  *
  */
 
-//Implementation from
-//https://www.techiedelight.com/queue-implementation-in-java/
+// Implementation from
+// https://www.techiedelight.com/queue-implementation-in-java/
 public class Queue {
 	private int front; // front points to front element in the queue
 	private int rear; // rear points to last element in the queue
 	private int capacity; // maximum capacity of the queue
 	private int count; // current size of the queue
 	private Object arr[]; // array to store queue elements
+	private int intArray[];
 
 	Queue() {
 		arr = new Object[64];
@@ -23,6 +24,14 @@ public class Queue {
 		front = 0;
 		rear = -1;
 		count = 0;
+	}
+	
+	Queue(int size){
+		capacity = 64;
+		front = 0;
+		rear = -1;
+		count = 0;
+		intArray = new int[size];
 	}
 
 	Object dequedObj = null;
@@ -43,16 +52,36 @@ public class Queue {
 	public void enqueue(Object item) {
 		// check for queue overflow
 		if (isFull()) {
-			// System.out.println("OverFlow\nProgram Terminated");
 		}
-
-		// System.out.println("Inserting " + item);
-
 		rear = (rear + 1);
 		arr[rear] = item;
 		count++;
 	}
 
+	public void enqueueInt(int item) {
+		// check for queue overflow
+		if (isFull()) {
+		}
+		rear = (rear + 1);
+		intArray[rear] = item;
+		count++;
+	}
+	
+	int dequedEle = 0;
+
+	public int dequeueInt() {
+
+		// check for queue underflow
+		if (isEmpty()) {
+			System.out.println("UnderFlow");
+		}
+		dequedObj = intArray[front];
+		// System.out.println("Removing " + arr[front]);
+		front = (front + 1);
+		count--;
+		return dequedEle;
+	}
+	
 	public int size() {
 		return count;
 	}
